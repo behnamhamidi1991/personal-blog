5810c3f5e60d51317c583b92ae271862c22dd389
behnamhamidi1991 <eslp91@hotmail.com>
2024-07-01 09:40:56 +0330

    load view and partial

---
+++ helpers.php
@@ -0,0 +1,45 @@
+<?php 
+
+
+/**
+ * Base Path
+ * @param string $path
+ * @return string $path
+ */
+function basePath($path = '') {
+    return __DIR__ . '/' . $path;
+}
+
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+    $viewPath = basePath("/views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        extract($data);
+        require $viewPath;
+    } else {
+        echo "There is no view with the name of {$name}";
+    }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name) {
+    $viewPartial = basePath("/views/partials/{$name}.php");
+
+    if (file_exists($viewPartial)) {
+        require $viewPartial;
+    } else {
+        echo "There is no partial with the name of {$name}";
+    }
+}
