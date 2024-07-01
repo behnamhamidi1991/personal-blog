933c2d32799fbc3106da9e972607aa6e7cdf472b
behnamhamidi1991 <eslp91@hotmail.com>
2024-07-01 12:59:50 +0330

    Check out

--- views/blog/create.view.php
+++ views/blog/create.view.php
@@ -1,3 +1,16 @@
-<?php 
+ 
+<?php loadPartial('head') ?>
+<?php loadPartial('navbar') ?>
+<?php loadPartial('second-top') ?>
 
-echo "Create Page";
+
+     <section id="form-container">
+        <form>
+            <input type="text" placeholder="Title ...">
+            <input type="text" placeholder="Category ...">
+            <textarea placeholder="Write your post here ..."></textarea>
+            <button type="submit">Submit</button>
+        </form>
+     </section>
+
+     <?php loadPartial('footer') ?>
