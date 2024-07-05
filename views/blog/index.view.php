f037a2904f99c6121d39cd475ccde3fb7db74459
behnamhamidi91 <eslp91@hotmail.com>
2024-07-05 17:08:43 +0330

    Blog page done

--- views/blog/index.view.php
+++ views/blog/index.view.php
@@ -11,83 +11,19 @@
         </div>
 
         <div class="blog-content">
-            <div class="blog-box">
-                <h2>JavaScript: First step for Frontend</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
-                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
-                    tenetur enim, obcaecati, autem ipsum?</p>
-                <div class="post-details">
-                    <span>11/02/2023</span>
-                    <span>Adam Divine</span>
-                    <span>Frontend</span>
-                </div>
-                <a href="#" class="blog-readmore-btn">Read More</a>
-            </div>
 
+            <?php foreach($posts as $post) : ?>
             <div class="blog-box">
-                <h2>PHP new features</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
-                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
-                    tenetur enim, obcaecati, autem ipsum?</p>
+                <h2><?= $post->title ?></h2>
+                <p><?= mb_substr($post->body, 0, 250) . ' ...' ?></p>
                 <div class="post-details">
-                    <span>18/02/2023</span>
-                    <span>Rebecca Albert</span>
-                    <span>Backend</span>
+                    <span> <?= $post->created_at ?></span>
+                    <span><?= $post->user_name ?></span>
+                    <span><?= $post->category ?></span>
                 </div>
                 <a href="#" class="blog-readmore-btn">Read More</a>
             </div>
-
-            <div class="blog-box">
-                <h2>Your ideal laptop for development</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
-                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
-                    tenetur enim, obcaecati, autem ipsum?</p>
-                <div class="post-details">
-                    <span>18/02/2023</span>
-                    <span>Rebecca Albert</span>
-                    <span>Backend</span>
-                </div>
-                <a href="#" class="blog-readmore-btn">Read More</a>
-            </div>
-
-            <div class="blog-box">
-                <h2>Why is Java Famous in Germany?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
-                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
-                    tenetur enim, obcaecati, autem ipsum?</p>
-                <div class="post-details">
-                    <span>11/02/2023</span>
-                    <span>Adam Divine</span>
-                    <span>Frontend</span>
-                </div>
-                <a href="#" class="blog-readmore-btn">Read More</a>
-            </div>
-
-            <div class="blog-box">
-                <h2>What is the main reason for errors?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
-                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
-                    tenetur enim, obcaecati, autem ipsum?</p>
-                <div class="post-details">
-                    <span>11/02/2023</span>
-                    <span>Adam Divine</span>
-                    <span>Frontend</span>
-                </div>
-                <a href="#" class="blog-readmore-btn">Read More</a>
-            </div>
-
-            <div class="blog-box">
-                <h2>Who can find a new job?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
-                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
-                    tenetur enim, obcaecati, autem ipsum?</p>
-                <div class="post-details">
-                    <span>11/02/2023</span>
-                    <span>Adam Divine</span>
-                    <span>Frontend</span>
-                </div>
-                <a href="#" class="blog-readmore-btn">Read More</a>
-            </div>
+            <?php endforeach ; ?>
 
             <a href="#" class="blog-read-more">Read More Posts</a>
         </div>
