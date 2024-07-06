a7b7818fa2fe94170ccea2132b2fad83712bd739
behnamhamidi91 <eslp91@hotmail.com>
2024-07-06 21:02:58 +0330

    Commit new files

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controllers;
 
+use Framework\Database;
+
 class HomeController {
+    
+    protected $db;
+    
     public function __construct() 
     {
-        die('HomeController@index');
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
     }
 
     public function index() {
-        
+        $posts = $this->db->query('SELECT posts.*, users.name AS user_name FROM posts INNER JOIN users ON posts.user_id = users.id LIMIT 6')->fetchAll();
+
+        loadView('home', [
+            'posts' => $posts
+        ]);
+
     }
 }
